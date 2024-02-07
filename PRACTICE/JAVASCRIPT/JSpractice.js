47c20167ea938f0844ac4581fc724844aecb8efd
MPA-SE <priscillamomodu@gmail.com>
2024-02-07 21:22:30 +0100

    chore(experimenting with a js exercice)

--- PRACTICE/JAVASCRIPT/JSpractice.js
+++ PRACTICE/JAVASCRIPT/JSpractice.js
@@ -1,1 +1,12 @@
-console.log("Welcome to JavaScript!");
+// print or display "I am a Software Engineer!" a hundred times
+const character = "I am a Software Engineer!"
+const timesToRepeat = 100;
+
+let final = " ";
+
+for (let i = 0; i < timesToRepeat; i++)
+{
+    final = final + character;
+}
+
+console.log(final);
